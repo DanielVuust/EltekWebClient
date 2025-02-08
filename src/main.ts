a90cb1dd14dfd7ce5ff75df305dd8cc0d552ee83
Daniel Nygaard Vuust28 <67711921+DanielVuust@users.noreply.github.com>
2025-02-08 13:25:35 +0100

    Summary

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { initializeApp } from '@angular/fire/app';
-import { environment } from './app/config/appsettings';
-
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
-  const app = initializeApp(environment.firebaseConfig);
